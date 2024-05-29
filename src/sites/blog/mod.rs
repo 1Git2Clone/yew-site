f6aa517a8cba471731abb7b09aa26f247b662b30
1Kill2Steal <ivankirilov640@gmail.com>
2024-05-29 17:38:34 +0300

    Replaced github gist import with godbolt

--- src/sites/blog/mod.rs
+++ src/sites/blog/mod.rs
@@ -4,7 +4,7 @@ pub mod data;
 pub mod utils;
 
 /// Utilities and data re-exports for convenience sake (used from the site modules)
-use crate::utils::{set_iframe_gist, set_youtube_iframe};
+use crate::utils::{set_iframe_godbolt, set_youtube_iframe};
 use {data::*, utils::*};
 
 /// Main and only blog navigation
